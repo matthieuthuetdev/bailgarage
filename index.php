f84be7f2cf3428b06571b6d02859e399e8cc6346
MatthieuThuet <98526173+matthieuthuetdev@users.noreply.github.com>
2025-02-19 17:03:33 +0100

    create the homepage

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ if(!empty($_SESSION)){
     require "./vue/menu.php";
 }
 require "./models/Database.php";
-
+require "./controllers/Homepage.php";
+require "./models/Database.php";
 
 
 require "./vue/footer.php";
