39919f97878fb13dc15a6db6575e582ed535fbc1
MatthieuThuet <98526173+matthieuthuetdev@users.noreply.github.com>
2025-02-20 12:09:48 +0100

    create condision for the menu and create the basic rooter

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <?php
 require "./vue/header.php";
-if(!empty($_SESSION)){
+if (empty($_SESSION)) {
     require "./vue/menu.php";
+} elseif ($_SESSION["right"] == "admin") {
+    require "./vue/adminMenu.php";
+}else {
+    require "./vue/ownerMenu.php";
 }
 require "./models/Database.php";
-require "./controllers/Homepage.php";
-require "./models/Database.php";
+require "./controllers/HomepageController.php";
+if (isset($_GET["pagecontroller"])) {
+    switch ($_GET["pagecontroller"]) {
+        case '':
+            $home = new HomepageController();
+            $home->displayHome();
+            break;
 
+        default:
+            $home = new HomepageController();
+            $home->displayHome();
+
+            break;
+    }
+} else {
+    $home = new HomepageController();
+    $home->displayHome();
+}
 
 require "./vue/footer.php";
