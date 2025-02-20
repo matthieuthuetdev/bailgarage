6e1533a5732d412dfa6fb6da0fe11bac54926add
MatthieuThuet <98526173+matthieuthuetdev@users.noreply.github.com>
2025-02-20 12:11:46 +0100

    create all menu for all type of right

---
+++ vue/adminMenu.php
@@ -0,0 +1,14 @@
+<ul>
+    <li>
+        <a href="#">Accueil</a>
+    </li>
+    <li>
+        <a href="#">Propriétaires</a>
+    </li>
+    <li>
+        <a href="#">Garage</a>
+    </li>
+    <li>
+        <a href="#">Profil</a>
+    </li>
+</ul>
---
+++ vue/menu.php
@@ -0,0 +1,5 @@
+<ul>
+    <li>
+        <a href="index.php?pagecontroller=user&action=signin">Se connecter</a>
+    </li>
+</ul>
