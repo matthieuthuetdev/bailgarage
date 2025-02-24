b159c8f7f696cb43ad225af9c93912b018da9ac6
MatthieuThuet <mthuet.pro@gmail.com>
2025-02-24 12:10:37 +0100

    add the garage controller and the vue to the rooter

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require "./models/Database.php";
 require "./models/Users.php";
 require "./controllers/HomepageController.php";
 require "./controllers/UserController.php";
+require "./controllers/GarageController.php";
 if (empty($_SESSION)) {
     require "./vue/menu.php";
 } elseif ($_SESSION["right"] == "admin") {
@@ -16,12 +17,23 @@ if (isset($_GET["pageController"])) {
         case "user":
             $user = new UserController();
             if (empty($_GET["action"])) {
-                $home = new HomepageController();
-                $home->displayHome();
+                header("location:index.php");
             } elseif ($_GET["action"] == "signIn") {
-            $user->displaySignInForm();
+                $user->displaySignInForm();
             }
             break;
+        case "garage":
+            $garage = new GarageController();
+            if (empty($_GET["action"])) {
+                header("location:index.php");
+                
+            } elseif ($_GET["action"] == "display") {
+                $garage->displayGarage();
+            }
+            break;
+
+
+
 
 
         default:
