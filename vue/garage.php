0a17771a965a863d506a64290859e54fd4994815
MatthieuThuet <mthuet.pro@gmail.com>
2025-02-25 10:25:00 +0100

    fix the title

--- vue/garage.php
+++ vue/garage.php
@@ -3,4 +3,7 @@ $message = $_SESSION["message"];
 $_SESSION["message"] = "";
 ?>
 <?php echo $message ?>
+<h1>Liste des garages</h1>
+<div>
 <span>le tableau avec la liste des garages sera afficher dès que j'aurais créé la fonction l'extraction de la base de donnée et l'affichage.</span>
+</div>
