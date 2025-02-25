699b84abfc53017054f247b3ce3ba8226b2085cb
MatthieuThuet <mthuet.pro@gmail.com>
2025-02-25 09:33:11 +0100

    fix link in the admin menu

--- vue/adminMenu.php
+++ vue/adminMenu.php
@@ -1,14 +1,14 @@
 <ul>
     <li>
-        <a href="#">Accueil</a>
+        <a href="index.php?pageController=user&action=display">propriétaires</a>
     </li>
     <li>
-        <a href="#">Propriétaires</a>
+        <a href="index.php?pageController=garage&action=display">Garage</a>
     </li>
     <li>
-        <a href="#">Garage</a>
+        <a href="index.php?pageController=user&action=profil">Profil</a>
     </li>
     <li>
-        <a href="#">Profil</a>
+        <a href="index.php?pageController=user&action=signOut">Se déconnecter</a>
     </li>
 </ul>
