d4735f0adc27e68e240801ee99245e5666a65f91
MatthieuThuet <mthuet.pro@gmail.com>
2025-02-25 09:39:53 +0100

    create a vue liste garage

--- vue/garage.php
+++ vue/garage.php
@@ -3,3 +3,4 @@ $message = $_SESSION["message"];
 $_SESSION["message"] = "";
 ?>
 <?php echo $message ?>
+<span>le tableau avec la liste des garages sera afficher dès que j'aurais créé la fonction l'extraction de la base de donnée et l'affichage.</span>
