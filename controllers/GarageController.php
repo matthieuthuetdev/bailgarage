030e4509e7ab464e76419b078a4b1408145e8405
MatthieuThuet <mthuet.pro@gmail.com>
2025-02-25 09:33:36 +0100

    add the vue liste

--- controllers/GarageController.php
+++ controllers/GarageController.php
@@ -3,6 +3,7 @@ class GarageController
 {
     public function __construct() {}
     public function displayGarage(): void {
+        $pageName = "Liste des garages";
         require "./vue/garage.php";
     }
 }
