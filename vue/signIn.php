b37265b44e71f455b9b0fdeaa8c07f4e786a409a
MatthieuThuet <mthuet.pro@gmail.com>
2025-02-25 10:39:38 +0100

    add a message if connection fails

--- vue/signIn.php
+++ vue/signIn.php
@@ -8,12 +8,14 @@ if (isset($_POST["email"], $_POST["password"])) {
         $_SESSION["firstName"] = $result["prenom"];
         $_SESSION["right"] = $result["role"];
         if ($_SESSION["right"] == "proprietaire") {
-            $_SESSION["message"] = "<span>Bienvenue ". $_SESSION["firstName"]. " vous êtes bien connecter ent tant que propriétaire.</span>";
+            $_SESSION["message"] = "<span>Bienvenue " . $_SESSION["firstName"] . " vous êtes bien connecter ent tant que propriétaire.</span>";
             header("location:index.php?pageController=garage&action=display");
-        }else {
-            $_SESSION["message"] = "<span>Bienvenue ". $_SESSION["firstName"]. " vous êtes bien connecter ent tant qu'administrateur.</span>";
+        } else {
+            $_SESSION["message"] = "<span>Bienvenue " . $_SESSION["firstName"] . " vous êtes bien connecter ent tant qu'administrateur.</span>";
             header("location: index.php?pageController=user&action=display");
         }
+    } else {
+        echo "adresse email ou le mot de passe incorect.";
     }
 }
 ?>
