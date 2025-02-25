d8819ad57d86f0d9eee152623783de8f3a983005
MatthieuThuet <mthuet.pro@gmail.com>
2025-02-25 12:19:43 +0100

    add a div

--- vue/owner.php
+++ vue/owner.php
@@ -4,4 +4,6 @@ $message = $_SESSION["message"];
 $_SESSION["message"] = "";
 ?>
 <?php echo $message ?>
-<span>le tableau avec la liste des propriétaire sera afficher dès que j'aurais créé la fonction l'extraction de la base de donnée et l'affichage.</span>
+<div>
+    <span>le tableau avec la liste des propriétaire sera afficher dès que j'aurais créé la fonction l'extraction de la base de donnée et l'affichage.</span>
+</div>
