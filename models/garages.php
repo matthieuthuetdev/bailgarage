374eabf38ac4ac922bb4526198926389c8279394
MatthieuThuet <mthuet.pro@gmail.com>
2025-03-19 10:57:52 +0100

    fix a probleme in the owner crud

--- models/garages.php
+++ models/garages.php
@@ -1,5 +1,5 @@
 <?php
-class Owners
+class Garages
 {
     private PDO $connection;
     public function __construct()
@@ -43,12 +43,12 @@ class Owners
     public function read($_id = null)
     {
         if (is_null($_id)) {
-            $request = "SELECT users.id AS userId, owners.id AS ownerId, users.name, users.firstName, users.email, owners.company, owners.address, owners.additionalAddress,citys.label, owners.phoneNumber, owners.iban, owners.bic, owners.attachmentPath, owners.gender, roles.name AS roleName FROM owners INNER JOIN users ON owners.userId = users.id INNER JOIN citys ON owners.cityId = citys.id INNER JOIN roles ON users.roleId = roles.id ";
+            $request = "SELECT users.id, users.name, users.firstName, users.email, owners.company, owners.address, owners.additionalAddress,citys.label, owners.phoneNumber, owners.iban, owners.bic, owners.attachmentPath, owners.gender, roles.name AS roleName FROM owners INNER JOIN users ON owners.userId = users.id INNER JOIN citys ON owners.cityId = citys.id INNER JOIN roles ON users.roleId = roles.id ";
             $rq = $this->connection->prepare($request);
             $rq->execute();
             $result = $rq->fetchAll(PDO::FETCH_ASSOC);
         } else {
-            $request = "SELECT users.id AS userId, owners.id AS ownerId, users.name, users.firstName, users.email, owners.company, owners.address, owners.additionalAddress,citys.label, owners.phoneNumber, owners.iban, owners.bic, owners.attachmentPath, owners.gender FROM owners INNER JOIN users ON owners.userId = users.id INNER JOIN citys ON owners.cityId = citys.id WHERE owners.id = :id";
+            $request = "SELECT users.name, users.firstName, users.email, owners.company, owners.address, owners.additionalAddress,citys.label, owners.phoneNumber, owners.iban, owners.bic, owners.attachmentPath, owners.gender FROM owners INNER JOIN users ON owners.userId = users.id INNER JOIN citys ON owners.cityId = citys.id WHERE users.id = :id";
             $rq = $this->connection->prepare($request);
             $rq->bindValue(":id", $_id, PDO::PARAM_INT);
             $rq->execute();
@@ -56,20 +56,19 @@ class Owners
         }
         return $result;
     }
-    public function update($_ownerId, $_name, $_firstName, $_email, $_company, $_address, $_additionalAddress, $_phoneNumber, $_iban, $_bic, $_attachmentPath, $_gender)
+    public function update($_userId, $_name, $_firstName, $_email, $_company, $_address, $_additionalAddress, $_phoneNumber, $_iban, $_bic, $_attachmentPath, $_gender)
     {
-        $userId = $this->read($_ownerId)["userId"];
         $request = "UPDATE users SET users.name = :name, users.firstName = :firstName, users.email = :email WHERE users.id = :userId";
         $rq = $this->connection->prepare($request);
-        $rq->bindValue(":userId", $userId, PDO::PARAM_INT);
+        $rq->bindValue(":userId", $_userId, PDO::PARAM_INT);
         $rq->bindValue(":email", $_email, PDO::PARAM_STR);
         $rq->bindValue(":name", $_name, PDO::PARAM_STR);
         $rq->bindValue(":firstName", $_firstName, PDO::PARAM_STR);
         $rq->execute();
 
-        $request = "UPDATE owners SET company = :company, address = :address, additionalAddress = :additionalAddress, phoneNumber = :phoneNumber, iban = :iban, bic = :bic, attachmentPath = :attachmentPath, gender = :gender WHERE owners.id =  :ownerId";
+        $request = "UPDATE owners SET company = :company, address = :address, additionalAddress = :additionalAddress, phoneNumber = :phoneNumber, iban = :iban, bic = :bic, attachmentPath = :attachmentPath, gender = :gender WHERE userId =  :userId";
         $rq = $this->connection->prepare($request);
-        $rq->bindValue(":ownerId", $_ownerId, PDO::PARAM_STR);
+        $rq->bindValue(":userId", $_userId, PDO::PARAM_INT);
         $rq->bindValue(":company", $_company, PDO::PARAM_STR);
         $rq->bindValue(":address", $_address, PDO::PARAM_STR);
         $rq->bindValue(":additionalAddress", $_additionalAddress, PDO::PARAM_STR);
@@ -81,12 +80,11 @@ class Owners
 
         return $rq->execute();
     }
-    public function delete($_ownerId)
+    public function delete($_userid)
     {
-        $userId =  $this->read($_ownerId)["userId"];
-        $request = "DELETE FROM owners WHERE id = :id";
+        $request = "DELETE FROM owners WHERE userId = :id";
         $rq =  $this->connection->prepare($request);
-        $rq->bindValue(":id", $_ownerId, PDO::PARAM_INT);
+        $rq->bindValue(":id", $_userid, PDO::PARAM_INT);
         $rq->execute();
         $request = "DELETE FROM users WHERE users.id = :id";
         $rq =  $this->connection->prepare($request);
