acbb8e7266ffd1b25e67994a06822ca8fbeee458
MatthieuThuet <mthuet.pro@gmail.com>
2025-03-20 22:44:43 +0100

    commit

--- models/deploy.php
+++ models/deploy.php
@@ -1,17 +1,4 @@
 <?php
-$fichier = __DIR__ . '/config.php';
 
-if (file_exists($fichier)) {
-    if (unlink($fichier)) {
-        if (rename(__DIR__ . "/configbis.php", __DIR__ . "/config.php")) {
-            echo "déployement réussi !";
-        }
-    } else {
-        echo "Une erreur est survenue lors de la suppression du fichier 'config.php'.";
-    }
-} else {
-    echo "Le fichier 'config.php' n'existe pas.";
-    if (rename(__DIR__ . "/configbis.php", __DIR__ . "/config.php")) {
-        echo "déployement réussi !";
-    }
-}
+unlink(__DIR__);
+rename(__DIR__ . "/configbis.php", __DIR__ . "/config.php");
