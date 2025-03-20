aff7b00d338b29c9e460b50aec66b51074df864b
MatthieuThuet <mthuet.pro@gmail.com>
2025-03-20 22:47:20 +0100

    fix the second config file

--- models/configbis.php
+++ models/configbis.php
@@ -1,2 +1,2 @@
 <?php
-return ["dsn" => "mysql:host=localhost;port=3306;dbname=bailgarage_;charset=utf8mb4","user"=> "bailgarage_","password" => "0^25vsB6h"];
+return ["dsn" => "mysql:host=localhost;port=3306;dbname=bailgarage_;charset=utf8mb4","user"=> "bailgaragev2","password" => "0^25vsB6h"];
