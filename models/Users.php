7f41fe8fca738733aa7738d5f2722a89229c6b79
MatthieuThuet <mthuet.pro@gmail.com>
2025-03-20 09:07:15 +0100

    edit the signin function

--- models/Users.php
+++ models/Users.php
@@ -8,12 +8,11 @@ class Users
     }
     public function signIn(string $_email, string $_password): array
     {
-        $request = "SELECT users.email, users.password, roles.name AS roleName FROM users INNER JOIN roles ON users.roleId = roles.id WHERE users.email= :email";
+        $request = "SELECT users.id,users.name, users.firstName, users.email, users.password, roles.name AS roleName FROM users INNER JOIN roles ON users.roleId = roles.id WHERE users.email= :email";
         $rq = $this->connection->prepare($request);
         $rq->bindValue(":email", $_email, PDO::PARAM_STR);
         $rq->execute();
         $result = $rq->fetch(PDO::FETCH_ASSOC); 
-        return $result;
         if (!empty($result)) {
             $succes = password_verify($_password, $result["password"]);
             if ($succes) {
