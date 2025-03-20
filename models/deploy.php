0a0e72ffea170ca077507416f7ed0da5cf16eaee
MatthieuThuet <mthuet.pro@gmail.com>
2025-03-20 22:36:05 +0100

    add a deployement file

---
+++ models/deploy.php
@@ -0,0 +1,15 @@
+<?php
+$fichier = __DIR__ . '/config.php';
+
+if (file_exists($fichier)) {
+    if (unlink($fichier)) {
+        if(rename(__DIR__."/configbis.php",__DIR__."/config.php")){
+            echo "déployement réussi !";
+        }
+    } else {
+        echo "Une erreur est survenue lors de la suppression du fichier 'config.php'.";
+    }
+} else {
+    echo "Le fichier 'config.php' n'existe pas.";
+}
+?>
