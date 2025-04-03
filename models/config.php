1494a0a0108bafa1ae528aacfdf8ae991c5e2316
MatthieuThuet <mthuet.pro@gmail.com>
2025-04-03 17:28:56 +0200

    change the config

--- models/config.php
+++ models/config.php
@@ -1,2 +1,2 @@
 <?php
-return ["dsn" => "mysql:host=localhost;port=3306;dbname=bailgaragev2;charset=utf8mb4","user"=> "root","password" => ""];
+return ["dsn" => "mysql:host=localhost;port=3306;dbname=bailgarage_;charset=utf8mb4","user"=> "bailgaragev2","password" => "0^25vsB6h"];
