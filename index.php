904cc180bc9d953d7b3df44ad02311b3826f7950
matthieuthuetdev <mthuet.pro@gmail.com>
2025-04-29 23:48:48 +0200

    delete function file

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require "./vendor/autoload.php";
-require "./function.php";
 use PHPMailer\PHPMailer\PHPMailer;
 
 require "./vue/header.php";
