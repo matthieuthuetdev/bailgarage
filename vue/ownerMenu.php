1c9fa55c762b828881e374262f00dd1a20b9872e
matthieuthuetdev <mthuet.pro@gmail.com>
2025-07-26 11:23:49 +0200

    implement menu with css

--- vue/ownerMenu.php
+++ vue/ownerMenu.php
@@ -1,23 +1,46 @@
-<ul>
-    <li>
-        <a href="index.php?pageController=garage&action=display">Garage</a>
-    </li>
-    <li>
-        <a href="index.php?pageController=tenant&action=display">Locataire</a>
-    </li>
-    <li>
-        <a href="index.php?pageController=lease&action=display">Baux</a>
-    </li>
-    <li>
-        <a href="index.php?pageController=payment&action=display">Demande de paiment</a>
-    </li>
-    <li>
-        <a href="index.php?pageController=paymenthistory&action=display">Historique des paiments</a>
-    </li>
-    <li>
-        <a href="index.php?pageController=user&action=profil">Profil</a>
-    </li>
-    <li>
-        <?php echo $_SESSION["role"] == "owner"?"<a href='index.php?pageController=user&action=signOut'>Se déconnecter</a>" : "<a href='index.php?pageController=owner&action=stophelp'>Sortire du mode aide</a>"; ?>
-    </li>
-</ul>
+<header>
+<link rel="stylesheet" href="./css/ownerMenu.css">
+    <aside class="sidebar">
+        <img src="./img/logo.png" alt="Logo" class="logo" />
+        <nav class="nav">
+            <h3>MENU</h3>
+            <ul>
+                <li>
+                    <a href="index.php?pageController=garage&action=display">
+                        <i class="fa-solid fa-warehouse"></i> GARAGE
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?pageController=tenant&action=display">
+                        <i class="fa-solid fa-users"></i> LOCATAIRES
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?pageController=lease&action=display">
+                        <i class="fa-solid fa-file-contract"></i> BAUX
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?pageController=payment&action=display">
+                        <i class="fa-solid fa-euro-sign"></i> DEMANDES DE PAIEMENT
+                    </a>
+                </li>
+                <li>
+                    <a href="index.?pageController=paymenthistory&action=display">
+                        <i class="fa-solid fa-clock-rotate-left"></i> HISTORIQUE DE PAIEMENT
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?pageController=user&action=profil">
+                        <i class="fa-solid fa-user"></i> PROFIL
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="logout-container">
+            <a href="index.php?pageController=user&action=signOut" class="logout">
+                <i class="fa-solid fa-arrow-right-from-bracket"></i> Se déconnecter
+            </a>
+        </div>
+    </aside>
+</header>
